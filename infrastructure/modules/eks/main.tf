7cbb3c909bde9da09215902f4a881e65d3c6541b
Raj <gururajrathod90@gmail.com>
2025-09-27 12:23:01 +0530

    replaced t3.small

--- infrastructure/modules/eks/main.tf
+++ infrastructure/modules/eks/main.tf
@@ -83,7 +83,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.private_subnet_ids
 
-  instance_types = ["t2.micro"]
+  instance_types = ["t3.small"]
   scaling_config {
     desired_size = 2
     max_size     = 3
